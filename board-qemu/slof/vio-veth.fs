11ce93b301dce1efff789885fc6c5282655073c2
Avik Sil <aviksil@linux.vnet.ibm.com>
2013-09-23 14:07:35 +0530

    Add veth driver in libveth
    Signed-off-by: Avik Sil <aviksil@linux.vnet.ibm.com>

--- board-qemu/slof/vio-veth.fs
+++ board-qemu/slof/vio-veth.fs
@@ -15,14 +15,41 @@
 " network" device-type
 
 INSTANCE VARIABLE obp-tftp-package
+0 VALUE veth-priv
+0 VALUE open-count
+
 : open  ( -- okay? )
-   my-unit 1 rtas-set-tce-bypass
-   my-args s" obp-tftp" $open-package obp-tftp-package ! true
+   open-count 0= IF
+      my-unit 1 rtas-set-tce-bypass
+      my-args s" obp-tftp" $open-package obp-tftp-package !
+      s" local-mac-address" get-node get-property not
+      s" reg" get-node get-property not 3 pick and IF
+         >r nip r>
+         libveth-open dup not IF ." libveth-open failed" EXIT THEN
+         drop TO veth-priv
+      THEN
+   THEN
+   open-count 1 + to open-count
+   true
 ;
 
 : close  ( -- )
-   s" close" obp-tftp-package @ $call-method
-   my-unit 0 rtas-set-tce-bypass
+   open-count 0> IF
+      open-count 1 - dup to open-count
+      0= IF
+         s" close" obp-tftp-package @ $call-method
+         veth-priv libveth-close
+         my-unit 0 rtas-set-tce-bypass
+      THEN
+   THEN
+;
+
+: read ( buf len -- actual )
+   veth-priv libveth-read
+;
+
+: write ( buf len -- actual )
+   veth-priv libveth-write
 ;
 
 : load  ( addr -- len )
