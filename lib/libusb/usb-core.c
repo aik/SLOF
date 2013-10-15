22c4d4b3ae3e971bba336e4e5d5ba3bd03dc4ade
Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>
2013-10-15 22:23:28 +0530

    usb-core: disable xhci
    Code still does not support xhci, disable creation of xhci node, as it
    was causing crash in the quiesce path.
    
    Also check if ops pointer is verified before accessing function
    pointers during usb_hcd_exit.
    
    Signed-off-by: Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>

--- lib/libusb/usb-core.c
+++ lib/libusb/usb-core.c
@@ -182,7 +182,7 @@ void usb_hcd_exit(void *_hcidev)
 	struct usb_hcd_dev *hcidev = _hcidev;
 
 	dprintf("%s: enter \n", __func__);
-	if (!hcidev) {
+	if (!hcidev || !hcidev->ops) {
 		printf("Device Error");
 		return;
 	}
