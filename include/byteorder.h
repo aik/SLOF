cb8725edfb1360973e40a5f1d51750346a2a961b
Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>
2016-02-01 11:17:54 +0530

    fix code style in byteorder.h
    Signed-off-by: Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

--- include/byteorder.h
+++ include/byteorder.h
@@ -19,38 +19,32 @@
 
 #include <stdint.h>
 
-static inline uint16_t
-bswap_16 (uint16_t x)
+static inline uint16_t bswap_16 (uint16_t x)
 {
 	return __builtin_bswap16(x);
 }
 
-static inline uint32_t
-bswap_32 (uint32_t x)
+static inline uint32_t bswap_32 (uint32_t x)
 {
 	return __builtin_bswap32(x);
 }
 
-static inline uint64_t
-bswap_64 (uint64_t x)
+static inline uint64_t bswap_64 (uint64_t x)
 {
 	return __builtin_bswap64(x);
 }
 
-static inline void
-bswap_16p (uint16_t *x)
+static inline void bswap_16p (uint16_t *x)
 {
 	*x = __builtin_bswap16(*x);
 }
 
-static inline void
-bswap_32p (uint32_t *x)
+static inline void bswap_32p (uint32_t *x)
 {
 	*x = __builtin_bswap32(*x);
 }
 
-static inline void
-bswap_64p (uint64_t *x)
+static inline void bswap_64p (uint64_t *x)
 {
 	*x = __builtin_bswap64(*x);
 }
