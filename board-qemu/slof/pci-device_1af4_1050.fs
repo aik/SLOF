38bf852e73ce6f0ac801dfe8ef1545c4cd0b5ddb
Thomas Huth <thuth@redhat.com>
2016-11-11 12:10:08 +0100

    Do not try to use virtio-gpu in VGA mode
    virtio-gpu and virtio-vga share the same PCI ID (but have a
    different PCI class). Since virtio-gpu does not have the VGA
    register interface anymore, we should never try to use these
    registers in SLOF, i.e. we must not include qemu-vga.fs in this
    case.
    
    Buglink: https://bugzilla.redhat.com/show_bug.cgi?id=1375166
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

--- board-qemu/slof/pci-device_1af4_1050.fs
+++ board-qemu/slof/pci-device_1af4_1050.fs
@@ -10,6 +10,14 @@
 \ *     IBM Corporation - initial implementation
 \ ****************************************************************************/
 
-s" virtio [ vga ]" type cr
+\ Note: The PCI ID 1af4:1050 is used for both, virtio-vga and virtio-gpu
+\ devices. Only the first one provides a VGA interface that we currently
+\ support in SLOF.
 
-s" qemu-vga.fs" included
+my-space pci-class@ 30000 = IF
+    s" virtio [ vga ]" type cr
+    s" qemu-vga.fs" included
+ELSE
+    s" virtio [ gpu ]" type cr
+    my-space pci-device-generic-setup
+THEN
