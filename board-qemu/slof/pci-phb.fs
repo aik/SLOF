aik/SLOF

commit e44b7f074f549f78303ad4d67d39b18db93d11bf

pci: Fix secondary and subordinate PCI bus enumeration with board-qemu
SLOF currently fails to correctly initialize the secondary and
subordinate bus number registers in the config space of PCI
bridges, so that for example with the following command line,
none of the PCI devices is usable:

 qemu-system-ppc64 -nodefaults -nographic -serial mon:stdio \
    -device pci-bridge,chassis_nr=1,id=bridge0,addr=0x3 \
    -device pci-bridge,chassis_nr=2,id=bridge1,addr=0x4 \
    -device virtio-balloon,bus=bridge1,addr=0x1 \
    -device virtio-net,bus=bridge0,addr=0x2 \
    -device virtio-rng,bus=bridge0,addr=0x5 \
    -device pci-bridge,chassis_nr=3,id=br2,addr=0x6,bus=bridge1 \
    -device e1000,bus=br2,addr=0x1

This is because SLOF tries to enumerate the PCI bus numbers
that are reachable via a bridge. In the function pci-bridge-probe,
it increases the pci-bus-number counter and writes that value into
the secondary bus number register of the PCI config space, and
after probing all attached bridges, it fills the number of the
last enumerated bus number into the subordinate bus number register.
This works fine if the whole bus enumeration is done by SLOF,
however on board-qemu, we nowadays rely on the pre-initialized PCI
device tree from QEMU - and the numbers that SLOF is trying to use
here do not match with the bus numbers that QEMU already assigned
to the bus segments (QEMU provides the device tree nodes in
descending order, but SLOF tries to enumerate the bus numbers in
ascending order here instead).

To fix this issue, we should simply stop setting up the secondary
and subordinate config space registers of the bridge in SLOF - since
this is done by QEMU already! Thus we replace the "pci-bridge-probe"
function with a board-qemu-specific function "phb-pci-bridge-probe",
that does not call "pci-bus!" and "pci-bus-subo!" anymore. (And since
pci-bridge-probe was the only spot that called phb-pci-probe-bus, we

board-qemu/slof/pci-phb.fs
@@ -304,9 +304,16 @@ setup-puid
     get-parent set-node
 ;
 
-\ Landing routing to probe the popuated device tree
-: phb-pci-probe-bus ( busnr -- )
-    drop phb-pci-walk-bridge
+\ Similar to pci-bridge-probe, but without setting the secondary and
+\ subordinate bus numbers (since this has been done by QEMU already)
+: phb-pci-bridge-probe ( addr -- )
+    dup pci-bridge-set-bases                      \ Set up all Base Registers
+    dup func-pci-bridge-range-props               \ Set up temporary "range"
+    pci-device-vec-len 1+ TO pci-device-vec-len   \ increase the device-slot vector depth
+    pci-enable                                    \ enable mem/IO transactions
+    phb-pci-walk-bridge                           \ and walk the secondary bus
+    pci-device-vec-len 1- TO pci-device-vec-len   \ decrease the device-slot vector depth
+    pci-bridge-set-limits                         \ Set up all Limit Registers
 ;
 
 \ Stub routine, as qemu has enumerated, we already have the device
@@ -328,7 +335,7 @@ setup-puid
        1 0 (probe-pci-host-bridge)
    ELSE
        2drop
-       ['] phb-pci-probe-bus TO func-pci-probe-bus
+       ['] phb-pci-bridge-probe TO func-pci-bridge-probe
        ['] phb-pci-device-props TO func-pci-device-props
        phb-pci-walk-bridge          \ PHB device tree is already populated.
    THEN

slof/fs/pci-properties.fs
@@ -643,7 +643,7 @@
         pci-device-slots >r             \ save the slot array on return stack
         dup pci-common-props            \ set the common properties before scanning the bus
         s" pci" device-type             \ the type is allways "pci"
-        dup pci-bridge-probe            \ find all device connected to it
+        dup func-pci-bridge-probe       \ find all device connected to it
         dup assign-all-bridge-bars      \ set up all memory access BARs
         dup pci-set-irq-line            \ set the interrupt pin
         dup pci-set-capabilities        \ set up the capabilities

slof/fs/pci-scan.fs
@@ -221,6 +221,8 @@ DEFER func-pci-bridge-range-props
         dup pci-bridge-set-limits                       \ SetUp all Limit Registers
         drop                                            \ forget the config-addr
 ;
+DEFER func-pci-bridge-probe
+' pci-bridge-probe TO func-pci-bridge-probe
 
 \ set up the pci-device
 : pci-device-setup ( addr -- )

commit 62674aabe20612a9786fa03e87cf6916ba97a99a

pci-phb: Set correct pci-bus-number while walking PCI bridges
Commit e44b7f07 ("Fix secondary and subordinate PCI bus enumeration")
created a board-qemu specific version of the pci-bridge-probe function
to fix problems with the secondary and  subordinate bus number
registers. Unfortunately, this function missed to update the
pci-bus-number variable like the original pci-bridge-probe function
did it, so that new bridges currently end up with a bad "my-bus"
setting (which is initialized from the pci-bus-number variable)
and thus things that depend on this setting, like the "decode-unit"
function currently don't work as expected on these PCI bridges.
Fix it by initializing the pci-bus-number from the PCI config
space settings that is provided by QEMU.

Fixes: e44b7f074f549f78303ad4d67d39b18db93d11bf
Buglink: https://bugzilla.redhat.com/show_bug.cgi?id=1459755
Signed-off-by: Thomas Huth <thuth@redhat.com>
Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

board-qemu/slof/pci-phb.fs
@@ -309,6 +309,7 @@ setup-puid
 : phb-pci-bridge-probe ( addr -- )
     dup pci-bridge-set-bases                      \ Set up all Base Registers
     dup func-pci-bridge-range-props               \ Set up temporary "range"
+    my-space pci-bus-scnd@ TO pci-bus-number      \ Set correct current bus number
     pci-device-vec-len 1+ TO pci-device-vec-len   \ increase the device-slot vector depth
     pci-enable                                    \ enable mem/IO transactions
     phb-pci-walk-bridge                           \ and walk the secondary bus