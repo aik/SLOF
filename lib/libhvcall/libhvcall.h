089fc18a9b8c38ff83d678f4ea05b270a172848c
Greg Kurz <groug@kaod.org>
2017-07-10 14:17:46 +0200

    libhvcall: drop unused KVMPPC_H_REPORT_MC_ERR and KVMPPC_H_NMI_MCE defines
    These defines were introduced to add support for FWNMI in KVM guests. This
    has been WIP since 2015 and the KVM part finally made it to kvm-ppc-next.
    
    Latest work on the QEMU side [1] no longer needs KVMPPC_H_REPORT_MC_ERR.
    The KVMPPC_HCALL_MAX define is never used anywhere, and moreover it
    is bogus since there's no KVMPPC_H_NMI_MCE private hcall.
    
    [1] http://lists.nongnu.org/archive/html/qemu-ppc/2015-12/msg00199.html
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

--- lib/libhvcall/libhvcall.h
+++ lib/libhvcall/libhvcall.h
@@ -25,8 +25,6 @@
 /* Client Architecture support */
 #define KVMPPC_H_CAS            (KVMPPC_HCALL_BASE + 0x2)
 #define KVMPPC_H_RTAS_UPDATE    (KVMPPC_HCALL_BASE + 0x3)
-#define KVMPPC_H_REPORT_MC_ERR  (KVMPPC_HCALL_BASE + 0x4)
-#define KVMPPC_HCALL_MAX        KVMPPC_H_NMI_MCE
 
 #ifndef __ASSEMBLY__
 
