16661f937c717e68962e3956fcd0b11198fca557
Thomas Huth <thuth@redhat.com>
2018-05-26 08:06:02 +0200

    libc: Add the snprintf() function
    Code has been taken from the sprintf() function (which is almost the same,
    except that snprintf calls vsnprintf instead of vsprintf internally).
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    [aik: fixed traling spaces]
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

--- lib/libc/include/stdio.h
+++ lib/libc/include/stdio.h
@@ -43,6 +43,7 @@ int fileno(FILE *stream);
 int printf(const char *format, ...) __attribute__((format (printf, 1, 2)));
 int fprintf(FILE *stream, const char *format, ...) __attribute__((format (printf, 2, 3)));
 int sprintf(char *str, const char *format, ...)  __attribute__((format (printf, 2, 3)));
+int snprintf(char *str, size_t size, const char *format, ...)  __attribute__((format (printf, 3, 4)));
 int vfprintf(FILE *stream, const char *format, va_list);
 int vsprintf(char *str, const char *format, va_list);
 int vsnprintf(char *str, size_t size, const char *format, va_list);
