aik/SLOF

commit cf28264196e596c11d0d47f6150dfef5c0e4239f

virtio-serial: Rework shutdown sequence
The "io" word of term-io.fs opens two separate instances of the device
for stdin and stdout. The prom_init() function in Linux closes stdin at
some point, which internally calls quiesce and shuts the device down
through a quiesce hook.

When the "open-count" variable in virtio-serial.fs reaches 0, ie. when
closing the last instance, we call "close" two times, which is clearly
wrong. This never hits however because the stdout instance is never
closed which prevents "open-count" to reach 0.

It would make more sense to shutdown the device when closing the last
instance, for symmetry with the first open that initializes the device.
Change the shutdown sequence to do that rather than relying on a quiesce
hook.

Have quiesce to explicitly close stdout, which is supposedly the last
instance, and shutdown the device.

Signed-off-by: Greg Kurz <groug@kaod.org>
Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

board-qemu/slof/virtio-serial.fs
@@ -19,13 +19,8 @@ virtio-setup-vd VALUE virtiodev
 \ Quiescence the virtqueue of this device so that no more background
 \ transactions can be pending.
 : shutdown  ( -- )
-    initialized? IF
-        my-phandle node>path open-dev ?dup IF
-            virtiodev virtio-serial-shutdown
-            close-dev
-        THEN
-        FALSE to initialized?
-    THEN
+    virtiodev virtio-serial-shutdown
+    FALSE to initialized?
 ;
 
 : virtio-serial-term-emit
@@ -39,7 +34,6 @@ virtio-setup-vd VALUE virtiodev
 : init  ( -- )
 virtiodev virtio-serial-init drop
     TRUE to initialized?
-    ['] shutdown add-quiesce-xt
 ;
 
 0 VALUE open-count
@@ -58,7 +52,7 @@ virtiodev virtio-serial-init drop
 : close
     open-count 0> IF
         open-count 1 - dup to open-count
-        0= IF close THEN
+        0= IF shutdown THEN
     THEN
     close
 ;

slof/fs/client.fs
@@ -203,6 +203,11 @@ ALSO client-voc DEFINITIONS
 	    \ End of life of SLOF now, call platform quiesce as quiesce
 	    \ is an undocumented extension and not everybody supports it
 	    close-dev
+	    \ Some device, eg. virtio-serial, need all instances to be
+	    \ closed in order to be reset properly
+	    s" stdout" get-chosen IF
+		decode-int nip nip close-dev
+	    THEN
 	    quiesce
 	ELSE
 	    close-dev

commit 76fee95890f7a80665a7cdb5eff73d064c141453

slof: Only close stdout for virtio-serial devices
Recent commit cf28264196e5 fixed an issue where a virtio-serial device
wouldn't shutdown properly during quiesce. The fix is to close stdout
just before quiesce. As expected this causes some messages to not
appear anymore, like the well known ones from prom_init():

Quiescing Open Firmware ...
Booting Linux via __start() @ 0x0000000002000000 ...

Actually all messages are discarded until the OS driver finally takes
control of the device, which may represent a fair amount of logging.
This is suboptimal but this still better than hanging in SLOF.

The hammer is a bit too big though because the change also affects
spapr-vty based consoles, which have no reason to stop working
after quiesce.

Move the hack from the common code to the virtio-serial code so that
it doesn't affect other device types anymore. Register a quiesce hook
that closes stdout in virtio-serial.fs.

While here, as suggested by Segher, bring back some robustness in the
shutdown method.

Reported-by: Fabiano Rosas <farosas@linux.ibm.com>
Fixes: cf28264196e5 "virtio-serial: Rework shutdown sequence"
Signed-off-by: Greg Kurz <groug@kaod.org>
Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

board-qemu/slof/virtio-serial.fs
@@ -19,9 +19,11 @@ virtio-setup-vd VALUE virtiodev
 \ Quiescence the virtqueue of this device so that no more background
 \ transactions can be pending.
 : shutdown  ( -- )
-    virtiodev virtio-serial-shutdown
-    FALSE to initialized?
-    0 to virtiodev
+    initialized? IF
+        virtiodev virtio-serial-shutdown
+        FALSE to initialized?
+        0 to virtiodev
+    THEN
 ;
 
 : virtio-serial-term-emit
@@ -31,10 +33,16 @@ virtio-setup-vd VALUE virtiodev
 : virtio-serial-term-key?  virtiodev virtio-serial-haschar ;
 : virtio-serial-term-key   BEGIN virtio-serial-term-key? UNTIL virtiodev virtio-serial-getchar ;
 
+: virtio-serial-close-stdout s" stdout" get-chosen IF decode-int nip nip close-dev THEN ;
+
 \ Basic device initialization - which has only to be done once
 : init  ( -- )
 virtiodev virtio-serial-init drop
     TRUE to initialized?
+    \ Linux closes stdin at some point in prom_init(). This internally triggers a
+    \ quiesce in SLOF. We must ensure stdout gets closed as well otherwise the
+    \ device cannot be reset properly and the boot will hang.
+    ['] virtio-serial-close-stdout add-quiesce-xt
 ;
 
 0 VALUE open-count

slof/fs/client.fs
@@ -203,11 +203,6 @@ ALSO client-voc DEFINITIONS
 	    \ End of life of SLOF now, call platform quiesce as quiesce
 	    \ is an undocumented extension and not everybody supports it
 	    close-dev
-	    \ Some device, eg. virtio-serial, need all instances to be
-	    \ closed in order to be reset properly
-	    s" stdout" get-chosen IF
-		decode-int nip nip close-dev
-	    THEN
 	    quiesce
 	ELSE
 	    close-dev